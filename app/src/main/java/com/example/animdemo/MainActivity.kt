ba3058945804a23b3d40ee9947e5211f1ea3c9b7
K1ycee <chiagozieani145@gmail.com>
2019-10-15 09:49:44 +0100

    Few animations for now commit

---
+++ app/src/main/java/com/example/animdemo/MainActivity.kt
@@ -0,0 +1,49 @@
+package com.example.animdemo
+
+import android.animation.AnimatorInflater
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun rotateAnimation(view: View) {
+
+        val rotateAnimator = AnimatorInflater.loadAnimator(this, R.animator.rotate)
+        rotateAnimator?.apply {
+            setTarget(targetImage)
+            start()
+        }
+    }
+
+    fun scaleAnimation(view: View) {
+
+        val scaleAnimator = AnimatorInflater.loadAnimator(this, R.animator.scale)
+        scaleAnimator?.apply {
+            setTarget(targetImage)
+            start()
+        }
+    }
+
+    fun translateAnimation(view: View) {
+
+        val translateAnimator = AnimatorInflater.loadAnimator(this, R.animator.translate)
+        translateAnimator.apply {
+            setTarget(targetImage)
+            start()
+        }
+    }
+
+    fun fadeAnimation(view: View) {
+
+        val fadeAnimator = AnimatorInflater.loadAnimator(this, R.animator.alpha)
+        fadeAnimator.setTarget(targetImage)
+        fadeAnimator.start()
+    }
+}
